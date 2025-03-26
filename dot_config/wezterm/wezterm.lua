c8198cd00d8785f8c039ae5de4877da431e46cbc
Martijn Rietdijk <martijn@rietdijk.it>
2025-03-26 08:49:40 +0100

    No longer using tmux or zellij

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,6 +9,55 @@ local function scheme_for_appearance(appearance)
 	end
 end
 
+local function is_vim(pane)
+	local process_name = pane:get_foreground_process_name()
+	return process_name == "nvim"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(key)
+	return {
+		key = key,
+		mods = "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				win:perform_action({ SendKey = { key = key, mods = "CTRL" } }, pane)
+			else
+				win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+			end
+		end),
+	}
+end
+
+config.leader = {
+	key = "s",
+	mods = "CTRL",
+}
+
+config.keys = {
+	split_nav("h"),
+	split_nav("j"),
+	split_nav("k"),
+	split_nav("l"),
+}
+
+-- config.keys = {
+-- 	{ key = "%", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+-- 	{ key = '"', mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+-- 	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+-- 	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+-- 	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+-- 	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+-- 	{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+-- 	{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+-- }
+
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 100
 
