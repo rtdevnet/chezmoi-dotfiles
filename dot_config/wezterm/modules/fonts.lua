ce6a28f6aab124a89c17a729b724eca04c706e9c
Martijn Rietdijk <martijn@rietdijk.it>
2025-05-05 14:46:43 +0200

    Minor updates

--- dot_config/wezterm/modules/fonts.lua
+++ dot_config/wezterm/modules/fonts.lua
@@ -4,15 +4,32 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.get_font()
-  local hostname = wezterm.hostname()
+	local hostname = wezterm.hostname()
 
-  if hostname == "powerbook" then
-    return wezterm.font("MonoLisa Nerd Font")
-  elseif hostname == "Workstation" then
-    return wezterm.font("JetBrainsMono Nerd Font")
-  else
-    return wezterm.font("Monaco")
-  end
+	if hostname == "powerbook" then
+		font = wezterm.font({
+			family = "Monaspace Neon",
+			harfbuzz_features = {
+				"calt",
+				"liga",
+				"dlig",
+				"ss01",
+				"ss02",
+				"ss03",
+				"ss04",
+				"ss05",
+				"ss06",
+				"ss07",
+				"ss08",
+			},
+			stretch = "UltraCondensed",
+		})
+		return font
+	elseif hostname == "Workstation" then
+		return wezterm.font("JetBrainsMono Nerd Font")
+	else
+		return wezterm.font("Monaco")
+	end
 end
 
 return M
