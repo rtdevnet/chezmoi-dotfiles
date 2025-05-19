9a3cb4cb57d8e0fbfe8cd0e078d79fb845fad783
Martijn Rietdijk <martijn@rietdijk.it>
2025-05-19 09:03:50 +0200

    Changed inital window size of Wezterm

--- dot_config/wezterm/modules/ui.lua
+++ dot_config/wezterm/modules/ui.lua
@@ -4,8 +4,8 @@ local M = {}
 function M.apply(config)
 	config.window_background_opacity = 0.95
 	config.macos_window_background_blur = 100
-	config.initial_rows = 160
-	config.initial_cols = 160
+	config.initial_cols = 150
+	config.initial_rows = 50
 	config.use_fancy_tab_bar = false
 
 	config.window_frame = {
